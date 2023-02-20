88f884e9efa25bd307bf5c93e13bf03b515fe18c
Samyak Bardiya <samyak.b@cumulations.com>
2023-02-20 18:46:46 +0530

    02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (sentence, times) {
+  if (times < 0) return 'ERROR';
 
+  let str = "";
+  for (let i = 0; i < times; ++i) {
+    str += sentence;
+  }
+  return str;
 };
 
 // Do not edit below this line
