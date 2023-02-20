0b28f6dd405cfbf71759d3932fbe6a8163c053dd
Samyak Bardiya <samyak.b@cumulations.com>
2023-02-20 19:14:47 +0530

    05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+  if (x < 0 || y < 0 || typeof x != "number" || typeof y != "number") {
+    return "ERROR";
+  }
 
+  let a = Math.min(x, y);
+  let b = Math.max(x, y);
+
+  let sum = 0;
+  for (let i = a; i <= b; i++) sum += i;
+
+  return sum;
 };
 
 // Do not edit below this line
