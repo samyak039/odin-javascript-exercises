56a584dac65828539e223513f1dd9c491fb95596
Samyak Bardiya <samyak.b@cumulations.com>
2023-02-21 12:10:04 +0530

    08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+  for (let num of arr) sum += num;
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let multiply = 1;
+  for (let num of arr) multiply *= num;
+  return multiply;
 };
 
-const power = function() {
-	
+const power = function (num, pow) {
+  let power = 1;
+  for (let i = 1; i <= pow; i++) power *= num;
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let fact = 1;
+  let i = 1;
+  while (i <= num) fact *= i++;
+  return fact;
 };
 
 // Do not edit below this line
@@ -29,5 +38,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
