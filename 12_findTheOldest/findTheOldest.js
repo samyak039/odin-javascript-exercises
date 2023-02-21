db927756acfc0f38aa16e5c68b034587e9f877b5
Samyak Bardiya <samyak.b@cumulations.com>
2023-02-21 13:08:30 +0530

    12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleArr) {
+  let ageArr = [];
+  let maxAge = 0;
+  let maxAgeIndex = 0;
 
+  for (const people of peopleArr) {
+    if (people["yearOfDeath"] != null) {
+      ageArr.push(people["yearOfDeath"] - people["yearOfBirth"]);
+    } else {
+      ageArr.push(2023 - people["yearOfBirth"]);
+    }
+  }
+
+  for (let i = 0; i < ageArr.length; i++) {
+    if (maxAge < ageArr[i]) {
+      maxAge = ageArr[i];
+      maxAgeIndex = i;
+    }
+  }
+
+  return peopleArr[maxAgeIndex];
 };
 
 // Do not edit below this line
