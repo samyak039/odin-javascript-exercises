68453e726e0722b8fbfd4392f48055dfe71d00f7
Samyak Bardiya <samyak.b@cumulations.com>
2023-02-21 12:33:58 +0530

    09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let removeItems = [" ", "!", ",", "."];
 
+  let arr = str.split("");
+  arr = arr.map((e) => e.toLowerCase());
+  arr = arr.filter((x) => !removeItems.includes(x));
+
+  let i = arr.length;
+  const a = [...arr];
+  const b = arr.reverse();
+
+  while (i--) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
