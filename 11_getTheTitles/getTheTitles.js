da4598777b9db70e8c0407c7ceddffa458db1e07
Samyak Bardiya <samyak.b@cumulations.com>
2023-02-21 12:59:41 +0530

    11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+  let titleArr = [];
 
+  for (let book of books) {
+    titleArr.push(book["title"]);
+  }
+
+  return titleArr;
 };
 
 // Do not edit below this line
