4c1943078fef713cb24ec1cfd1cf341e65fb69ef
Samyak Bardiya <samyak.b@cumulations.com>
2023-02-21 12:52:08 +0530

    10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function (fib) {
+  if (fib < 0) return "OOPS";
 
+  let fibArr = [1, 1];
+  for (let i = 2; i < fib; i++) fibArr[i] = fibArr[i - 1] + fibArr[i - 2];
+
+  return fibArr.at(fib - 1);
 };
 
 // Do not edit below this line
